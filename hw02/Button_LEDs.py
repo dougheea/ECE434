7a8de84ac456a6a92bc022463da7a51435880f70
Emily Dougherty <dougheea@rose-hulman.edu>
2020-09-10 20:58:03 -0400

    and now im going to try if statements

--- hw02/Button_LEDs.py
+++ hw02/Button_LEDs.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 #chmod +x etch_a_sketch.py
+
+
 import Adafruit_BBIO.GPIO as GPIO
 import time
+#setting up the LEDS as inputs and the buttons as outputs
 GPIO.setup("P9_12", GPIO.OUT)
 GPIO.setup("P9_14", GPIO.OUT)
 GPIO.setup("P9_20", GPIO.OUT)
@@ -11,30 +14,30 @@ GPIO.setup("P9_11", GPIO.IN)
 GPIO.setup("P9_13", GPIO.IN)
 GPIO.setup("P9_15", GPIO.IN)
 GPIO.setup("P9_17", GPIO.IN)
-
+]#initating a count to keep track if the LED should be turned on or off
 count1 = 0
 count2 = 0
 count3 = 0
 count4 = 0
 
 while(1):
-    if GPIO.input("P9_11"):
-        if count1 ==0:
-            time.sleep(0.1)
-            GPIO.output("P9_12", GPIO.HIGH)
-            count1 += 1
+    if GPIO.input("P9_11"): #if this button is triggered then the count is checked
+        if count1 == 0: # count value of zero means the LED is currently off
+            time.sleep(0.1) 
+            GPIO.output("P9_12", GPIO.HIGH) #LED is turned on
+            count1 += 1 #count is changed to reflect the state of LED
         else:
             time.sleep(0.1)
-            GPIO.output("P9_12", GPIO.LOW)
-            count1 -= 1
+            GPIO.output("P9_12", GPIO.LOW) #LED is turned off
+            count1 -= 1 #count is changed to reflect the state of LED
     if GPIO.input("P9_13"):
         if count2 ==0:
             time.sleep(0.1)
-            GPIO.output("P9_14", GPIO.HIGH)
+            GPIO.output("P9_20", GPIO.HIGH)
             count2 += 1
         else:
             time.sleep(0.1)
-            GPIO.output("P9_14", GPIO.LOW)
+            GPIO.output("P9_20", GPIO.LOW)
             count2 -=1
     if GPIO.input("P9_15",):
         if count3 ==0:
