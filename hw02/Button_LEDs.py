ddc5875f6bd7b3bd4f95472f2e41dc975ddd8e52
Emily Dougherty <dougheea@rose-hulman.edu>
2020-09-10 20:29:13 -0400

    trying while loops for buttons

--- hw02/Button_LEDs.py
+++ hw02/Button_LEDs.py
@@ -3,8 +3,25 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 GPIO.setup("P9_12", GPIO.OUT)
-GPIO.output("P9_12", GPIO.LOW)
-time.sleep(2)
-GPIO.output("P9_12", GPIO.HIGH)
+GPIO.setup("P9_14", GPIO.OUT)
+GPIO.setup("P9_16", GPIO.OUT)
+GPIO.setup("P9_18", GPIO.OUT)
+
+GPIO.setup("P9_11", GPIO.IN)
+GPIO.setup("P9_13", GPIO.IN)
+GPIO.setup("P9_15", GPIO.IN)
+GPIO.setup("P9_17", GPIO.IN)
+
+while(1):
+    while(GPIO.input("P9_11")):
+        GPIO.output("P9_12")
+    while(GPIO.input("P9_13")):
+        GPIO.output("P9_14")
+    while(GPIO.input("P9_15")):
+        GPIO.output("P9_16")
+    while(GPIO.input("P9_17")):
+        GPIO.output("P9_18")
+    
+
 
 GPIO.cleanup()
