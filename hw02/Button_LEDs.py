d9da4a7a1f4da84f11431b1c461d1c3c68900700
Emily Dougherty <dougheea@rose-hulman.edu>
2020-09-10 21:00:01 -0400

    and now im going to try if statements

--- hw02/Button_LEDs.py
+++ hw02/Button_LEDs.py
@@ -7,7 +7,7 @@ import time
 #setting up the LEDS as inputs and the buttons as outputs
 GPIO.setup("P9_12", GPIO.OUT)
 GPIO.setup("P9_14", GPIO.OUT)
-GPIO.setup("P9_20", GPIO.OUT)
+GPIO.setup("P9_16", GPIO.OUT)
 GPIO.setup("P9_18", GPIO.OUT)
 
 GPIO.setup("P9_11", GPIO.IN)
@@ -33,20 +33,20 @@ while(1):
     if GPIO.input("P9_13"):
         if count2 ==0:
             time.sleep(0.1)
-            GPIO.output("P9_20", GPIO.HIGH)
+            GPIO.output("P9_14", GPIO.HIGH)
             count2 += 1
         else:
             time.sleep(0.1)
-            GPIO.output("P9_20", GPIO.LOW)
+            GPIO.output("P9_14", GPIO.LOW)
             count2 -=1
     if GPIO.input("P9_15",):
         if count3 ==0:
             time.sleep(0.1)
-            GPIO.output("P9_20", GPIO.HIGH)
+            GPIO.output("P9_16", GPIO.HIGH)
             count3 +=1
         else:
             time.sleep(0.1)
-            GPIO.output("P9_20", GPIO.LOW)
+            GPIO.output("P9_16", GPIO.LOW)
             count3 -= 1
     if GPIO.input("P9_17"):
         if count4 ==0:
