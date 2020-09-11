efa03080333beed9662ce3adbf2986ec71d28622
Emily Dougherty <dougheea@rose-hulman.edu>
2020-09-10 20:21:50 -0400

    changed sleep

--- hw02/Button_LEDs.py
+++ hw02/Button_LEDs.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 #chmod +x etch_a_sketch.py
 import Adafruit_BBIO.GPIO as GPIO
-
+import time
 GPIO.setup("P9_12", GPIO.OUT)
 GPIO.output("P9_12", GPIO.LOW)
-sleep(1)
+time.sleep(2)
 GPIO.output("P9_12", GPIO.HIGH)
 
 GPIO.cleanup()
