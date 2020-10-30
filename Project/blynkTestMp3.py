4e3d96cf26f8fbe25276ee261afe3817446b2fac
Demo User <debian@beaglebone.localdomain>
2020-10-30 19:22:46 +0000

    thread structure

--- Project/blynkTestMp3.py
+++ Project/blynkTestMp3.py
@@ -3,6 +3,9 @@ import blynklib
 import blynktimer
 import sys
 import os
+import logging
+import threading
+import time
 
 # Get the autherization code (See setup.sh)
 BLYNK_AUTH = os.getenv('BLYNK_AUTH')
@@ -10,11 +13,28 @@ BLYNK_AUTH = os.getenv('BLYNK_AUTH')
 # Initialize Blynk
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
+
+def song1Lights():
+    print("BLINKY BLINKY TIME")
+    time.sleep(60)
+    print("finished 60 sec")
+    time.sleep(120)
+    print("finished 3 min")
+
+
 @blynk.handle_event('write V0')
-def allIWantForChristmas(pin, value):
+def song1(pin, value):
+    testThread = threading.Thread(target=song1Lights)
+    testThread.start()
     print("Playing Mariah Carey's 'All I Want For Christmas is You'")
-    os.system("mplayer -ao alsa:device=sysdefault=Set AllIWantForChristmas.mp3")
+    os.system("mplayer -ao alsa:device=sysdefault=Set hozier.mp3")
+    print("finished playing")
+        
+
     
-while True:
-    blynk.run()
+if __name__ == "__main__":
+    while True:
+        blynk.run()
+    print("all done")
+   
    
